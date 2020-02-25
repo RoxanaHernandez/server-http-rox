9793bf9ccb619c3f15a2932cf8c8a2bf63f03bd2
RoxanaHernandez <roxanahernandez326@gmail.com>
2020-02-25 19:29:01 -0300

    ok

--- indedx.js
+++ indedx.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = 8080;
 app.get('/',(req, res) => res.send('hola'));
+app.get('/numero', (req, res)=>{
+    return res.send(Math.floor(Math.random()*1000).toString());
+});
+
 
 
 app.listen(port, ()=> console.log(`servidor corriendo en puerto: ${port}`));
